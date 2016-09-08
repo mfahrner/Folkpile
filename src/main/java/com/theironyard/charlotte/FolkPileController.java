f523f966acd60309bd28564f4b889b8f147d5808
Ben Sterrett <raynjamin@gmail.com>
2016-09-08 13:40:39 -0400

    Changing around RequestMapping a bit.

--- src/main/java/com/theironyard/charlotte/FolkPileController.java
+++ src/main/java/com/theironyard/charlotte/FolkPileController.java
@@ -3,13 +3,15 @@ package com.theironyard.charlotte;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by Ben on 9/8/16.
  */
 @Controller
 public class FolkPileController {
-    @RequestMapping(path = "/", method = RequestMethod.GET)
+    @RequestMapping(path = "/")
+    @ResponseBody
     String home() {
         return "Hello, World!";
     }
