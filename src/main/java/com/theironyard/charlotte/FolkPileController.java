6c05b79376396d41bb6cbd1b7f5b1967573e1328
michael fahrner <Mike Fahrner>
2016-09-08 15:29:09 -0400

    so I added stuff too

--- src/main/java/com/theironyard/charlotte/FolkPileController.java
+++ src/main/java/com/theironyard/charlotte/FolkPileController.java
@@ -1,9 +1,16 @@
 package com.theironyard.charlotte;
 
+import com.theironyard.charlotte.entities.Person;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * Created by Ben on 9/8/16.
@@ -14,5 +21,25 @@ public class FolkPileController {
     @ResponseBody
     String home() {
         return "Hello, World!";
+    }
+
+    @Autowired
+    PersonRepository people;
+
+    @PostConstruct
+       public void init() throws IOException {
+        if (people.count() == 0) {
+            File f = new File("folkpile.csv");
+            Scanner fileScanner = new Scanner(f);
+
+            while (fileScanner.hasNext()) {
+
+                String line = fileScanner.nextLine();
+                String[] columns = line.split(",");
+
+                Person person = new Person(columns[0], columns[1], columns[2], columns[3], columns[4], columns[5]);
+                people.save(person);
+            }
+        }
     }
 }
