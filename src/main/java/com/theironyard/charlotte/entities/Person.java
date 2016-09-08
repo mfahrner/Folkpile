a1dcc2c7cb9c0ec7abbf7e988eee3e0a8a10ef6d
wrodgers95 <will.rodgers1995@gmail.com>
2016-09-08 15:24:49 -0400

    added Person class and PersonRepository

--- src/main/java/com/theironyard/charlotte/entities/Person.java
+++ src/main/java/com/theironyard/charlotte/entities/Person.java
@@ -8,8 +8,24 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "people")
 public class Person {
+
     @Id
     @GeneratedValue
     Integer id;
 
+    String firstName;
+    String lastName;
+    String userName;
+    String gender;
+    String birthday;
+    String photo;
+
+    public Person(String firstName, String lastName, String userName, String gender, String birthday, String photo) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.gender = gender;
+        this.birthday = birthday;
+        this.photo = photo;
+    }
 }
